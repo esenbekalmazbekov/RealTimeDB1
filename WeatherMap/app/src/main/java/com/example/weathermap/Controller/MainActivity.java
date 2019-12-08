7427567d7f2314aba664e0093cdf96972a07f29e
esenbek <esenbek.almazbekov@gmail.com>
2019-12-09 00:47:34 +0600

    second beta version

--- WeatherMap/app/src/main/java/com/example/weathermap/Controller/MainActivity.java
+++ WeatherMap/app/src/main/java/com/example/weathermap/Controller/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.weathermap.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,6 +16,8 @@ import com.example.weathermap.Models.Convert;
 import com.example.weathermap.Models.getfromapi.CityWeather;
 import com.example.weathermap.Models.WeatherApi;
 import com.example.weathermap.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -33,7 +40,11 @@ public class MainActivity extends AppCompatActivity{
     private static final String TOKYO_CITY_KEY = "226396";
     private static final String NEWYORK_CITY_KEY = "349727";
     private static final String CAIRO_CITY_KEY = "127164";
-    // Our City data
+
+    //Google Connects
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +52,41 @@ public class MainActivity extends AppCompatActivity{
 
         findIds();
         pasteData();
+
+        if(isServicesOK())
+            init();
+    }
+
+    private void init(){
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     private void findIds(){
--- WeatherMap/app/src/main/java/com/example/weathermap/Models/WeatherApi.java
+++ WeatherMap/app/src/main/java/com/example/weathermap/Models/WeatherApi.java
@@ -9,6 +9,6 @@ import retrofit2.http.Path;
 public interface WeatherApi {
     String DOMAIN = "https://dataservice.accuweather.com/";
 
-    @GET( "forecasts/v1/daily/5day/{cityKey}?apikey=in5iNkbzCVLoyJBsWEsHAxDCBXOc2B2R")
+    @GET( "forecasts/v1/daily/5day/{cityKey}?apikey=8Oq7OgXNCLTbPvO29SG9kGDCB3efVMjQ")
     Call<CityWeather> getData(@Path("cityKey") String cityKey);
 }
