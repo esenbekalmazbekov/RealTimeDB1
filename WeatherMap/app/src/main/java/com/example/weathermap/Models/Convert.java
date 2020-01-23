372ce3e9b53970b70053504a1c2d44b157f0289b
esenbek <esenbek.almazbekov@gmail.com>
2020-01-24 02:33:46 +0600

    Firebase1

--- WeatherMap/app/src/main/java/com/example/weathermap/Models/Convert.java
+++
@@ -1,9 +0,0 @@
-package com.example.weathermap.Models;
-
-public class Convert {
-    private static Integer answer;
-    public static Integer convert(Integer ans){
-        answer = (( 5 *(ans - 32)) / 9);
-        return answer;
-    }
-}
